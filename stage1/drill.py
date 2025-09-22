a0037d775562f5faa4696a7d9236007f85e7fc13
javeri-a <javeriaqureshi746@gmail.com>
2025-09-22 18:38:03 +0500

    add day 01

---
+++ stage1/drill.py
@@ -0,0 +1,7 @@
+# This program adds two numbers provided by the user
+
+num1 = int(input("Enter your 1st number:"))
+num2 = int(input("Enter your 2nd number:"))
+
+print("the sum is:", num1 + num2)
+print("successfully added!")
