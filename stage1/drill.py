7b485f2438dfd1f2b737b82141421670ea2a936d
javeri-a <javeriaqureshi746@gmail.com>
2025-09-22 18:53:13 +0500

    odd

--- stage1/drill.py
+++ stage1/drill.py
@@ -9,5 +9,14 @@ print("successfully added!")
 
 name = input("Enter your name:")
 city = input("Enter your city:")
-print(name, "lives in",city) # using commas
-print ("hello! my name is" +name +"and Iam from" + city) # string concatenation
+print(name, "lives in", city) # using commas
+print("hello! my name is " + name + " and  am from " + city) # string concatenation
+
+
+
+# Even or Odd
+num  = int(input("Enter a number:"))
+if num % 2 == 0:
+    print(num, "is an Even number")
+else:
+    print(num, "is an Odd number")
