20c0080b27969dd9fee8d44af039d01b6de3095f
javeri-a <javeriaqureshi746@gmail.com>
2025-09-23 20:33:33 +0500

    add file

--- stage1/data types/string.py
+++ stage1/data types/string.py
@@ -34,4 +34,56 @@ a = 1
 b = 2
 c= "5"
 print (a + b + int(c) ) #8
-print (a + b + c ) # Error
+# print (a + b + c ) # Error
+
+
+
+# String Operations
+
+# Concatenation (join):
+first_name = "Javeria"
+last_name = "Qureshi"
+full_name = first_name + " " + last_name  # Space bhi add kiya
+print(full_name)  # Output: Javeria Qureshi
+
+#REPITITION
+print ("I LOVE HAZRAT MUHAMMAD SALLALAHU ALAIHI WASSALAM ❤ " * 100)
+
+
+# Indexing (single character):
+name = "Javeria"
+print(name[0])  # Output: J (first character)
+print(name[-1])  # Output: a (last character)
+print(name[2])  # Output: v (third character)
+print(name[-3])  # Output: r (third last character)
+print(name[1])  # Output: a (second character)
+
+
+# Slicing (substring):
+
+print("python"[0:3])   # Pyt
+print("python "[2:])    # thon
+
+
+# Apna naam ek variable me store karo aur uska first letter print karo.
+
+my_name = "Javeria"
+print(my_name[0])  # Output: J
+
+# Apni favourite city ka naam likho aur usko uppercase aur lowercase me print karo.
+favcity = "Makkah  and Madinah"
+print (favcity.upper())
+print (favcity.lower())
+
+
+# Ek sentence lo "I love Python" aur usme "Python" ko "Coding" se replace karo.
+sentence = "I love Python❤"
+new_sentence = sentence.replace("Python", "Coding")
+print(new_sentence)  # Output: I love Coding
+
+
+
+
+# Ek string "abcdefg" lo aur uska sirf middle part print karo
+new2:str = "abcefghijklmnopqrstuvwxyz"
+print (new2 [8:18])
