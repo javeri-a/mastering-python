4af90ee9a37a32378c515d62aca4055e60cddd3f
javeri-a <javeriaqureshi746@gmail.com>
2025-09-23 11:04:55 +0500

    add 2nd day

---
+++ stage1/day2.py
@@ -0,0 +1,25 @@
+
+age : int = 25
+name : str = "Javeria"
+
+
+
+#ek variable name banao jisme tumhara naam ho. Usko print karo.
+name1 = input("Enter your name:")
+print("my name is",name1)
+
+#Ek variable age banao, type annotation ke sath (int). Print karo.
+age1  = int(input("Enter your age:")) 
+print(age1)
+
+#input() use karke user se ek number lo aur uska square print karo.
+num1 = int(input("Enter your 1st number:"))
+num2 = int(input("Enter your 2nd number:"))
+result = num1 **2
+print("the result is:", result)
+
+#Do numbers input lo aur unka sum print karo.
+
+num3 = int(input("Enter your 1st number:"))
+num4 = int(input("Enter your 2nd number:"))
+print("the sum is:", num3 + num4)
