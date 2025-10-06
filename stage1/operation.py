39ee431dedd1d64a44a908a2a96c11cbacccd2e8
javeri-a <javeriaqureshi746@gmail.com>
2025-10-06 19:53:41 +0500

    add operation

--- stage1/operation.py
+++ stage1/operation.py
@@ -42,3 +42,19 @@ b = 5
 print(a > 3 and b < 10)  # True if both true
 print(a > 20 or b < 10)  # True if any one true
 print(not(a > 3))        # Reverse the result
+
+
+
+# is → check karta hai kya dono variables same memory object ko point kar rahe hain.
+
+# == → check karta hai kya dono variables ke values same hain
+
+
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = a
+
+print(a == b)  # True  (values same)
+print(a is b)  # False (different memory)
+print(a is c)  # True  (same object)
