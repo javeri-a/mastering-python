519c81d6365512d4eb43a8b03e5c9ca92977202d
javeri-a <javeriaqureshi746@gmail.com>
2025-10-06 19:40:21 +0500

    add operations

---
+++ stage1/operation.py
@@ -0,0 +1,35 @@
+# . What are Operators?
+
+# Operators are symbols that perform actions on values or variables.
+
+#  Types of Operators
+# Arithmetic Operators
+
+# Used for basic math.
+
+a = 10
+b = 3
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division (gives float)
+print(a // b) # Floor division (removes decimal)
+print(a % b)  # Modulus (remainder)
+print(a ** b) # Exponentiation (power)
+
+
+
+# Comparison Operators
+
+# Compare two values, return True or False.
+
+x = 10
+y = 5
+
+print(x == y)  # Equal to
+print(x != y)  # Not equal to
+print(x > y)   # Greater than
+print(x < y)   # Less than
+print(x >= y)  # Greater or equal
+print(x <= y)  # Less or equal
